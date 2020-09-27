3ee394d609be746385939447fc25c79087c61b34
Youseop <66366941+youseop@users.noreply.github.com>
2020-09-27 11:00:12 +0900

    .

--- BAEKJOON/9471_피사노주기.py
+++ BAEKJOON/9471_피사노주기.py
@@ -1,30 +1,29 @@
 import sys
 sys.stdin = open('input.txt', 'rt')
 
-n = int(input())
 
-
-def m_fibo(x, n):
+def pisano(x):
     if len(fibo) > x:
         return fibo[x]
     else:
-        fibo[x] = (m_fibo(x-1, n)+m_fibo(x-2, n)) % n
+        fibo[x] = (pisano(x-1)+pisano(x-2)) % m
         return fibo[x]
 
 
-def mode(n):
+def cycle():
     x = 1
     while True:
         if fibo[x] == 0 and fibo[x-1] == 1:
             return x
         x += 1
-        m_fibo(x, n)
+        pisano(x)
 
 
-for _ in range(n):
+for _ in range(int(input())):
     fibo = dict()
     fibo[0] = 0
     fibo[1] = 1
     fibo[2] = 1
-    a, n = map(int, sys.stdin.readline().split())
-    print(a, mode(n))
+    a, m = map(int, sys.stdin.readline().split())
+
+    print(a, cycle())
