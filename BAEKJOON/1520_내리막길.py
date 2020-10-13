bf02146f6f4dc2f5bc809e5c4a7aaf6e6dc71e58
Youseop <66366941+youseop@users.noreply.github.com>
2020-10-14 00:44:27 +0900

    Update 1520_내리막길.py

--- BAEKJOON/1520_내리막길.py
+++ BAEKJOON/1520_내리막길.py
@@ -1,3 +1,36 @@
+import sys
+sys.stdin = open("input.txt", "rt")
+
+read = sys.stdin.readline
+sys.setrecursionlimit(1000000000)  # 중요!
+m, n = map(int, read().split())
+stairs = list(list(map(int, read().split())) for _ in range(m))
+
+dp = list(list(-1 for _ in range(n)) for _ in range(m))
+dp[-1][-1] = 1
+
+dx = [0, -1, 0, 1]
+dy = [1, 0, -1, 0]
+
+
+def dfs(i, j):
+    if dp[i][j] != -1:
+        return dp[i][j]
+
+    dp[i][j] = 0
+
+    for x, y in zip(dx, dy):
+        if 0 <= i+x < m and 0 <= j+y < n and stairs[i+x][j+y] < stairs[i][j]:
+            dp[i][j] += dfs(i+x, j+y)
+    # for x in dp:
+    #     print(*x)
+    # print()
+    return dp[i][j]
+
+
+print(dfs(0, 0))
+
+
 # 시행착오!!
 '''
 import sys
