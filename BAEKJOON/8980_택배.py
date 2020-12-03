b2c962088636596718181c9ab94a4d2f79305e37
Youseop <66366941+youseop@users.noreply.github.com>
2020-12-03 22:52:09 +0900

    Update 8980_택배.py

--- BAEKJOON/8980_택배.py
+++ BAEKJOON/8980_택배.py
@@ -1,14 +1,19 @@
+
 import sys
 read = sys.stdin.readline
 
 n, c = map(int, read().split())
 m = int(read())
 things = list(list(map(int, read().split())) for _ in range(m))
+# 물건의 도착지가 빠른 순으로 정렬
 things.sort(key=lambda x: (x[1], x[0]))
-
+# 해당 마을에서 물건을 얼마나 더 실을 수 있는지 확인
 left = list(c for _ in range(n+1))
+
 answer = 0
+
 for a, b, w in things:
+    # a ~ b마을에서 물건을 더 실을 수 있는 값들의 최소값
     available = min(left[a:b])
     if available > 0:
         put = min(available, w)
