6268d82641c915718a0a276f8158002a13a663dc
Youseop <66366941+youseop@users.noreply.github.com>
2020-12-07 00:14:54 +0900

    Update 13913_숨바꼭질.py

--- BAEKJOON/13913_숨바꼭질.py
+++ BAEKJOON/13913_숨바꼭질.py
@@ -12,32 +12,31 @@ path[n]=-1
 #BFS
 point=deque([n])
 while point:
-    for _ in range(len(point)):
-        a=point.popleft()
-        #동생을 찾은 경우 시간과 경로를 출력하고 시스템을 종료한다.
-        if a==k:
-            print(dp[a])
-            way=[]
-            #경로 복원
-            while True:
-                way.append(a)
-                a=path[a]
-                if a == -1:
-                    print(*way[::-1])
-                    break
-            sys.exit()
-        else:
-            if a-1>=0 and dp[a-1]==-1:
-                dp[a-1]=dp[a]+1
-                path[a-1]=a
-                point.append(a-1)
-            if a+1<=k*2 and dp[a+1]==-1:
-                dp[a+1]=dp[a]+1
-                path[a+1]=a
-                point.append(a+1)
-            if a*2<=k*2 and dp[a*2]==-1:
-                dp[a*2]=dp[a]+1
-                path[a*2]=a
-                point.append(a*2)
+    a=point.popleft()
+    #동생을 찾은 경우 시간과 경로를 출력하고 시스템을 종료한다.
+    if a==k:
+        print(dp[a])
+        way=[]
+        #경로 복원
+        while True:
+            way.append(a)
+            a=path[a]
+            if a == -1:
+                print(*way[::-1])
+                break
+        sys.exit()
+    else:
+        if a-1>=0 and dp[a-1]==-1:
+            dp[a-1]=dp[a]+1
+            path[a-1]=a
+            point.append(a-1)
+        if a+1<=k*2 and dp[a+1]==-1:
+            dp[a+1]=dp[a]+1
+            path[a+1]=a
+            point.append(a+1)
+        if a*2<=k*2 and dp[a*2]==-1:
+            dp[a*2]=dp[a]+1
+            path[a*2]=a
+            point.append(a*2)
 
 
