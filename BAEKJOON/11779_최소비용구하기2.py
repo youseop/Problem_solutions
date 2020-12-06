2b9afe0fdef9889acdaca798df91d86d7580c6d2
Youseop <66366941+youseop@users.noreply.github.com>
2020-12-07 00:18:36 +0900

    Update 11779_최소비용구하기2.py

--- BAEKJOON/11779_최소비용구하기2.py
+++ BAEKJOON/11779_최소비용구하기2.py
@@ -16,12 +16,14 @@ for _ in range(m):
 
 start,end=map(int,read().split())
 start,end=start-1,end-1
-
+#이전 노드 저장
 way=list(-1 for _ in range(n))
+#start로부터 최단거리 저장
 dp=list(inf for _ in range(n))
 dp[start]=0
 
 point=[[0, start]]
+#heapq를 사용해 아직 방문하지 않은 점들 중 최단거리의 점 탐색
 hq.heapify(point)
 
 while point:
@@ -32,10 +34,14 @@ while point:
     if a in bridge:
         for x, w_x in bridge[a]:
             if dp[x] > w_a + w_x:
+                #x의 최단거리 업데이트
                 dp[x] = w_a + w_x
+                #이전 노드 저장
                 way[x] = a
+                #heapq에 노드 추가
                 hq.heappush(point,[dp[x], x])
 
+#경로 복원
 memo_way=[]
 while True:
     memo_way.append(a+1)
