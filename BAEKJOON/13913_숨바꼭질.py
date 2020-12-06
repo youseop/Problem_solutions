53546923f3f7d5c04840043c49c71afae265cb94
Youseop <66366941+youseop@users.noreply.github.com>
2020-12-06 14:22:35 +0900

    Create 13913_숨바꼭질.py

---
+++ BAEKJOON/13913_숨바꼭질.py
@@ -0,0 +1,43 @@
+import sys
+from collections import deque
+read=sys.stdin.readline
+
+n,k=map(int,read().split())
+#해당 좌표까지 이동하는데 걸리는 시간 저장
+dp=list(-1 for _ in range(max(n,k)*2+1))
+dp[n]=0
+#해당 좌표로 오기 이전의 좌표 저장(경로 확인 용도)
+path=list(0 for _ in range(max(n,k)*2+1))
+path[n]=-1
+#BFS
+point=deque([n])
+while point:
+    for _ in range(len(point)):
+        a=point.popleft()
+        #동생을 찾은 경우 시간과 경로를 출력하고 시스템을 종료한다.
+        if a==k:
+            print(dp[a])
+            way=[]
+            #경로 복원
+            while True:
+                way.append(a)
+                a=path[a]
+                if a == -1:
+                    print(*way[::-1])
+                    break
+            sys.exit()
+        else:
+            if a-1>=0 and dp[a-1]==-1:
+                dp[a-1]=dp[a]+1
+                path[a-1]=a
+                point.append(a-1)
+            if a+1<=k*2 and dp[a+1]==-1:
+                dp[a+1]=dp[a]+1
+                path[a+1]=a
+                point.append(a+1)
+            if a*2<=k*2 and dp[a*2]==-1:
+                dp[a*2]=dp[a]+1
+                path[a*2]=a
+                point.append(a*2)
+
+
