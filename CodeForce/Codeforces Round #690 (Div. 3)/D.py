a27fe810b81d266dc9bf1505da3cd9701f788873
Youseop <66366941+youseop@users.noreply.github.com>
2020-12-16 10:52:54 +0900

    Update D.py

---
+++ CodeForce/Codeforces Round #690 (Div. 3)/D.py
@@ -0,0 +1,21 @@
+import sys, math
+sys.stdin = open("TextFile1.txt","rt")
+read=sys.stdin.readline
+from collections import deque
+
+def find(arr):
+    sum_arr = sum(arr)
+
+
+
+    return
+
+
+
+N = int(read())
+
+for _ in range(N):
+    n = int(read())
+    num=list(map(int,read().split()))
+    
+    find(num)
