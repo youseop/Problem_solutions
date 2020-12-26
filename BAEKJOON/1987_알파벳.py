d17f5c4e275b849d201dc69c9c65efb5cc5c38e6
Youseop <66366941+youseop@users.noreply.github.com>
2020-12-27 00:45:39 +0900

    .

---
+++ BAEKJOON/1987_알파벳.py
@@ -0,0 +1,20 @@
+import sys
+from collections import deque
+read=sys.stdin.readline
+
+n,m = map(int,read().split())
+board = list(list(read().strip()) for _ in range(n))
+def BFS():
+    cnt = 0
+    point = set()
+    point.add((0,0,board[0][0]))
+    while point:
+        a,b, way = point.pop()
+        cnt = max(cnt, len(way))
+        for x,y in [(1,0),(0,1),(-1,0),(0,-1)]:
+            ax,by = a+x,b+y
+            if 0<=ax<n and 0<=by<m and board[ax][by] not in way:
+                point.add((ax,by,way+board[ax][by]))
+    return cnt
+
+print(BFS())
