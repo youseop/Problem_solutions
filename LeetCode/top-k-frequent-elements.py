8bd1773db63588eda8863246058093b4fa67cad0
youseop <edlsw@naver.com>
2021-04-30 13:58:34 +0900

    top-k-frequent-elements

---
+++ LeetCode/top-k-frequent-elements.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        dictionary = dict()
+        for n in nums:
+            if n not in dictionary:
+                dictionary[n] = 1
+            else:
+                dictionary[n] += 1
+        return sorted(dictionary, key=lambda x: -dictionary[x])[:k]
+        
