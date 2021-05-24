7684afc1a9b84469790afb2edfa852e2e397a148
youseop <edlsw@naver.com>
2021-05-24 22:32:57 +0900

    촌수계산 다시풀기

--- BAEKJOON/2644_촌수계산.py
+++ BAEKJOON/2644_촌수계산.py
@@ -32,3 +32,42 @@ for _ in range(int(read())):
     bridge[b].append(a)
 
 print(bfs())
+
+
+
+'''
+import sys
+sys.stdin = open("text.txt","rt")
+read = sys.stdin.readline
+
+from collections import deque
+
+n = int(read())
+a,b = map(int,read().split())
+
+bridge = list([] for _ in range(n+1))
+
+for _ in range(int(read())):
+    x,y = map(int,read().split())
+    bridge[x].append(y)
+    bridge[y].append(x)
+
+point = deque([a])
+count = 0
+check = list(True for _ in range(n+1))
+while point:
+    for _ in range(len(point)):
+        p = point.popleft()
+        if p == b:
+            print(count)
+            exit()
+
+        for next_p in bridge[p]:
+            if check[next_p]:
+                check[next_p] = False
+                point.append(next_p)
+
+    count += 1
+
+print(-1)
+'''
